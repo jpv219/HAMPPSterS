3a16fdaddeab58f854685bd04101db270ce89d80
Juan <j.valdes20@imperial.ac.uk>
2023-07-11 19:03:15 +0100

    No words

--- LSTM-RNN/Clean_CSV.py
+++ LSTM-RNN/Clean_CSV.py
@@ -5,15 +5,22 @@ warnings.filterwarnings('ignore')
 
 def clean_csv(df,columns):
 
-    if columns[1] != 'Volume':
-        label_list = list(df.columns.values)[0:3]
-        df.rename(columns={label_list[0]: 'Time'}, inplace=True)
-        df.rename(columns={label_list[1]: 'Volume'}, inplace=True)
-        df.rename(columns={label_list[2]: 'Gammatilde'}, inplace=True)
-        columns = list(df.columns.values)[1:3]
+    if len(columns) != 1:
 
-    for column in columns:
-        df[column] = df[column].str.replace('[','').str.replace(']','').str.split(' ')
-        df[column] = df[column].apply(lambda x: [i for i in x if i != ''])
-        df[column] = df[column].apply(lambda x: np.array([float(i) for i in x]))
+        if columns[1] != 'Volume':
+            label_list = list(df.columns.values)[0:3]
+            df.rename(columns={label_list[0]: 'Time'}, inplace=True)
+            df.rename(columns={label_list[1]: 'Volume'}, inplace=True)
+            df.rename(columns={label_list[2]: 'Gammatilde'}, inplace=True)
+            columns = list(df.columns.values)[1:3]
+
+        for column in columns:
+            df[column] = df[column].str.replace('[','').str.replace(']','').str.split(' ')
+            df[column] = df[column].apply(lambda x: [i for i in x if i != ''])
+            df[column] = df[column].apply(lambda x: np.array([float(i) for i in x]))
+    else:
+        for column in columns:
+            df[column] = df[column].str.replace('[','').str.replace(']','').str.split(' ')
+            df[column] = df[column].apply(lambda x: [i for i in x if i != ''])
+            df[column] = df[column].apply(lambda x: np.array([float(i) for i in x]))
     return df
