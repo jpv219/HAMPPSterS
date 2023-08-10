72908ff8ca13fe2463559aa9924dfbd38f032786
Paula Pico <pdp19@ce-pdp19.ce.ic.ac.uk>
2023-08-10 13:22:18 +0100

    Initial Commit

---
+++ Database-ActiveLearning/general_monitoring.py
@@ -0,0 +1,6 @@
+### Tailored for Imperial College's HPC
+### General monitoring and restarting for BLUE jobs
+### to be run locally
+### Author: Paula D. Pico
+### First commit: Aug, 2023
+### Department of Chemical Engineering, Imperial College London
